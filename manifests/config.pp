3cb48f7f68ac5f01a0999b41a427e3047d0cd701
Frederik Wagner <wagner@wagit.de>
2014-05-19 11:10:15 +0200

    added dokuwiki plugindir to writable dirs

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,8 @@ class dokuwiki::config (
   $version_string = "dokuwiki-${version}"
 
   file { ["${basedir}/${version_string}/conf",
-          "${basedir}/${version_string}/data"]:
+          "${basedir}/${version_string}/data",
+          "${basedir}/${version_string}/lib/plugins"]:
     ensure  => directory,
     recurse => true,
     owner   => $dokuwiki::params::www_owner,
