a6fe6115cd24c1fc2cf3881403e378eafd43d777
Robin H. Johnson <robin.johnson@bc.libraries.coop>
2014-11-23 00:23:34 -0800

    manifests/config: Do not recursively track data directory, causes problems on large deploys.
    Signed-off-by: Robin H. Johnson <robin.johnson@bc.libraries.coop>

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,12 +7,19 @@ class dokuwiki::config (
   $version_string = "dokuwiki-${version}"
 
   file { ["${basedir}/${version_string}/conf",
-          "${basedir}/${version_string}/data",
           "${basedir}/${version_string}/lib/plugins",
           "${basedir}/${version_string}/lib/tpl"]:
     ensure  => directory,
     recurse => true,
     owner   => $dokuwiki::params::www_owner,
+  }
+  # If this resource is made recursive, you can get a huge report on each run:
+  # ~750 bytes per file, and dokuwiki keeps old revisions in files. BCLibCoop
+  # private wiki caused a 450MB growth in the report :-(.
+  file { "${basedir}/${version_string}/data":
+    ensure  => directory,
+    owner   => $dokuwiki::params::www_owner,
+    recurse => false,
   }
 
 }
